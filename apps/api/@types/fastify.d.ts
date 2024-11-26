9e34c7d7f71712d7da12a7c353eb728c13144c0d
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-11-26 20:49:14 -0300

    feat: add middleware function for get organization

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import { Member, Organization } from '@prisma/client'
+
 declare module 'fastify' {
 	interface FastifyRequest {
 		getCurrentUserId(): Promise<string>
+		getUserMemeberShip(
+			slug: string,
+		): Promise<{ organization: Organization; membership: Member }>
 	}
 }
