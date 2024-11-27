1b79f7fe240fba5fde07c184b14385b4ab00129f
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-11-26 21:14:21 -0300

    feat: create routes findMany and findOne organization

--- apps/api/src/http/routes/orgs/get-organization.ts
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -1,30 +1,35 @@
-import { Role } from '@prisma/client'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 import { auth } from '@/http/middlewares/auth'
 
-export async function getMembership(app: FastifyInstance) {
+export async function getOrganization(app: FastifyInstance) {
 	app
 		.withTypeProvider<ZodTypeProvider>()
 		.register(auth)
 		.get(
-			'/organizations/:slug/membership',
+			'/organiztions/:slug',
 			{
 				schema: {
 					tags: ['organizations'],
-					summary: 'Get user membership on organization',
+					summary: 'Get details from organizations',
 					security: [{ bearerAuth: [] }],
 					params: z.object({
 						slug: z.string(),
 					}),
 					response: {
 						200: z.object({
-							membership: z.object({
-								id: z.string(),
-								role: z.nativeEnum(Role),
-								organizationId: z.string(),
+							organization: z.object({
+								id: z.string().uuid(),
+								name: z.string(),
+								slug: z.string(),
+								domain: z.string().nullish(),
+								shouldAttachUsersByDomain: z.boolean(),
+								avatarUrl: z.string().nullish(),
+								createdAt: z.date(),
+								updatedAt: z.date(),
+								ownerId: z.string().uuid(),
 							}),
 						}),
 					},
@@ -32,13 +37,10 @@ export async function getMembership(app: FastifyInstance) {
 			},
 			async (request, reply) => {
 				const { slug } = request.params
-				const { membership } = await request.getUserMemeberShip(slug)
+				const { organization } = await request.getUserMemeberShip(slug)
+
 				return reply.status(200).send({
-					membership: {
-						id: membership.id,
-						role: membership.role,
-						organizationId: membership.organizationId,
-					},
+					organization,
 				})
 			},
 		)
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,8 @@ import { requestPassowrdRecover } from './routes/auth/request-password-recover'
 import { resetPassowrd } from './routes/auth/reset-password'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -66,6 +68,8 @@ app.register(requestPassowrdRecover)
 app.register(resetPassowrd)
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
 	console.log('Server is running on http://localhost:3000')
