ccc79f9bda3ef88a0d1a3983973be894fe60ad78
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-11-29 00:06:19 -0300

    feat: routes to accept/reject organization's invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,7 +18,11 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPassowrdRecover } from './routes/auth/request-password-recover'
 import { resetPassowrd } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -94,6 +98,10 @@ app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
 app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
 	console.log('Server is running on http://localhost:3000')
